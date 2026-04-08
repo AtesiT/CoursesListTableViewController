bc02adc3afa4bad048312d775718d4187223e16c
AtesiT <atesit@mail.ru>
2026-04-08 17:08:58 +0300

    ADD: TableVC and CellVC

---
+++ CoursesListTableViewController/ViewControllers/TableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
---
+++ CoursesListTableViewController/ViewControllers/TableViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+final class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
