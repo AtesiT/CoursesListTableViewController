58671a4821d40b782636dde8ac1e4a9ccbb32cb2
AtesiT <atesit@mail.ru>
2026-04-09 17:05:24 +0300

    ADD: ViewController (that was made for display a Info)

--- CoursesListTableViewController/ViewControllers/ViewController.swift
+++ CoursesListTableViewController/ViewControllers/ViewController.swift
@@ -2,8 +2,45 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessonsLabel: UILabel!
+    @IBOutlet var numberOfTestLabel: UILabel!
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var favoriteButton: UIButton!
+    
+//    var course: Course!
+    var course: Product!
+    
+    private var isFavorite = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func toggleFavorite() {
+        isFavorite.toggle()
+        setStatusForFavoriteButton()
+        DataManager.shared.setFavoriteStatus(for: course.name, with: isFavorite)
+    }
+    
+    private func setupUI() {
+        courseNameLabel.text = course.name
+        numberOfLessonsLabel.text = "Number of lessons: \(course.numberOfTests)"
+        numberOfTestLabel.text = "Number of tests: \(course.numberOfTests)"
+        
+        if let imageData = ImageManager.shared.fetchImageData(from: course.imageURL) {
+            courseImage.image = UIImage(data: imageData)
+        }
+        
+        setStatusForFavoriteButton()
+    }
+    
+    private func setStatusForFavoriteButton() {
+        favoriteButton.tintColor = isFavorite ? .red : .gray
+    }
+    
+    private func loadFavoriteStatus() {
+        isFavorite = DataManager.shared.getFavoriteStatus(for: course.name)
     }
 }
