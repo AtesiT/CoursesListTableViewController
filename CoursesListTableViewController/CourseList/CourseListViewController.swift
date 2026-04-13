dfccca136e1338f3e76a9e81fb0015374a79d8f3
AtesiT <atesit@mail.ru>
2026-04-13 15:59:43 +0300

    EDIT: CourseListVC

--- CoursesListTableViewController/CourseList/CourseListViewController.swift
+++ CoursesListTableViewController/CourseList/CourseListViewController.swift
@@ -6,30 +6,27 @@ final class CourseListViewController: UIViewController {
     
     private var activityIndicator: UIActivityIndicatorView?
     
-    //    private var courses: [Course] = []
-    private var courses: [Product] = []
+    private var viewModel: CourseListViewModelProtcol! {
+        didSet {
+            viewModel.fetchCourses { [weak self] in
+                //  Так как это начальный VC, то необязательно список захвата реализовывать
+                self?.tableView.reloadData()
+                self?.activityIndicator?.stopAnimating()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = CourseListViewModel()
         tableView.rowHeight = 100
         activityIndicator = showActivityIndicator(in: view)
         setupNavigationBar()
-        getCourses()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let detailVC = segue.destination as? CourseDetailsViewController else { return }
         detailVC.course = sender as? Product
-    }
-    
-    private func getCourses() {
-        NetworkManager.shared.fetchData() { courses in
-            self.courses = courses
-            DispatchQueue.main.async {
-                self.activityIndicator?.stopAnimating()
-                self.tableView.reloadData()
-            }
-        }
     }
     
     private func setupNavigationBar() {
@@ -57,13 +54,13 @@ final class CourseListViewController: UIViewController {
 //  MARK: - TableViewDataSource
 extension CourseListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        courses.count
+        viewModel.courses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CourseCell", for: indexPath)
         guard let cell = cell as? CourseCell else { return UITableViewCell() }
-        let course = courses[indexPath.row]
+        let course = viewModel.courses[indexPath.row]
         cell.configure(with: course)
         
         return cell
@@ -74,7 +71,7 @@ extension CourseListViewController: UITableViewDataSource {
 extension CourseListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let course = courses[indexPath.row]
+        let course = viewModel.courses[indexPath.row]
         performSegue(withIdentifier: "showDetails", sender: course)
     }
 }
