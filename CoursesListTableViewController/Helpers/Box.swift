042c2a4a92ebd8002b0a4a50402824ff76cb701b
AtesiT <atesit@mail.ru>
2026-04-16 16:53:40 +0300

    UPDATE: Class box

--- CoursesListTableViewController/Helpers/Box.swift
+++ CoursesListTableViewController/Helpers/Box.swift
@@ -1,11 +1,25 @@
 import Foundation
 
 class Box<T> {
-    var value: T
+    //  Делаем typealias для работы как с обычным типом данных
+    typealias Listener = (T) -> Void
     
-    private var listener: ((T) -> Void)?
+    var value: T {
+        //  Как только меняется значение value, то мы обращаемся к listener и помещаем обновленное значение
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    private var listener: Listener?
     
     init(value: T) {
         self.value = value
+    }
+    
+    //  Возвращает наш value
+    func bind(listener: @escaping Listener) {
+        self.listener = listener
+        listener(value)
     }
 }
