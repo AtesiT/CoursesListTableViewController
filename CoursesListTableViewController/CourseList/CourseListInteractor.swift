1e4e797776d374d46e04814027e153366c22982c
AtesiT <atesit@mail.ru>
2026-04-24 14:34:37 +0300

    ADD: CourseListInteractor

--- CoursesListTableViewController/CourseList/CourseListInteractor.swift
+++ CoursesListTableViewController/CourseList/CourseListInteractor.swift
@@ -1,3 +1,22 @@
-class CourseListInteractor {
+import Foundation
+
+protocol CourseListInteractorInputProtocol {
+    init(presenter: CourseListInteractorOutputProtocol)
+    func fetchCourses()
+}
+
+protocol CourseListInteractorOutputProtocol {
+    func coursesDidReceive(with dataStore: CourseListDataStore)
+}
+
+class CourseListInteractor: CourseListInteractorInputProtocol {
+    private unowned let presenter: CourseListInteractorOutputProtocol
     
+    required init(presenter: CourseListInteractorOutputProtocol) {
+        self.presenter = presenter
+    }
+    
+    func fetchCourses() {
+        
+    }
 }
