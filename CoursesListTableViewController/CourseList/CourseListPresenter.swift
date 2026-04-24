5acddec07be0f926ac23895501d89c7144cc2a9a
AtesiT <atesit@mail.ru>
2026-04-24 14:24:40 +0300

    ADD: CourseListPresenter

--- CoursesListTableViewController/CourseList/CourseListPresenter.swift
+++ CoursesListTableViewController/CourseList/CourseListPresenter.swift
@@ -1,3 +1,29 @@
-class CourseListPresenter {
+import Foundation
+
+struct CourseListDataStore {
+    let courses: [Course]
+}
+
+
+class CourseListPresenter: CourseListViewOutputProtocol {
+    var interactor: CourseListInteractorInputProtocol!
+    var router: CourseListRouterInputProtocol!
     
+    private unowned let view: CourseListViewInputProtcol!
+    private var dataStore: CourseListDataStore?
+    
+    required init(view: CourseListViewInputProtcol) {
+        self.view = view
+    }
+    
+    func viewDidLoad() {
+        interactor.fetchCourses()
+    }
+}
+
+//  MARK: - CourseListInteractorOutputProtocol
+extension CourseListPresenter: CourseListInteractorOutputProtocol {
+    func coursesDidReceive(with dataStore: CourseListDataStore) {
+        
+    }
 }
